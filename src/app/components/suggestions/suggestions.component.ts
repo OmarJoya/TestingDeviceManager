fedb6485fd2a24652251e9dd4df6dd1edf6a8544
Omar Joya <ojoyar@gmail.com>
2018-11-14 00:18:54 -0600

    adding margin to main container

--- src/app/components/suggestions/suggestions.component.ts
+++ src/app/components/suggestions/suggestions.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-suggestions',
-  templateUrl: './suggestions.component.html'
 })
+templateUrl: './suggestions.component.html'
 export class SuggestionsComponent implements OnInit {
 
   constructor() { }
